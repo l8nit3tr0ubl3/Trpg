3eb1415deeb9fd754819c6923f0ef65a7e5fdf8d
l8nit3 <l8nit3@outlook.com>
2019-12-01 01:59:26 -0500

    Bugfixes

--- creatures.py
+++ creatures.py
@@ -2,14 +2,15 @@
 Creature definitions for enemies, multiply values by lvl#
 """
 class Monster():
-    def __init__(self, species, health, weapon, lvl, gil):
+    def __init__(self, species, health, attack, weapon, lvl, gil):
         self.species = species
         self.health = health
+        self.attack = attack
         self.weapon = weapon
         self.lvl = lvl
         self.gil = gil
 
-Troll = Monster("Troll", 1, "Small Axe", 1, 1)
-Elf = Monster("Elf", 3, "Bow", 1, 5)
-Dwarf = Monster("Dwarf", 5, "Battle Axe", 1, 10)
-Giant = Monster("Giant", 10, "Boulder", 1, 100)
+Troll = Monster("Troll", 1, 1, "Small Axe", 1, 1)
+Elf = Monster("Elf", 3, 1, "Bow", 1, 5)
+Dwarf = Monster("Dwarf", 5, 3, "Battle Axe", 1, 10)
+Giant = Monster("Giant", 10, 5, "Boulder", 1, 100)
